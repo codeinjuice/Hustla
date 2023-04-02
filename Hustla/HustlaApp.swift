3f40bf9591afab4ac385b1d24840cabd5b9ab16c
옥민재 <>
2023-04-02 16:14:07 +0900

    Project Created

---
+++ Hustla/HustlaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HustlaApp.swift
+//  Hustla
+//
+//  Created by MJ Oak on 2023/04/02.
+//
+
+import SwiftUI
+
+@main
+struct HustlaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
