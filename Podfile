7bd496eb95c2f4494127536b2acca3926f210556
옥민재 <>
2023-04-02 16:15:20 +0900

    Cocoapod init

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '13.0'
+
+target 'Hustla' do
+  use_frameworks!
+
+  pod 'Alamofire'
+  pod 'Kingfisher', '~> 7.0'
+end
---
+++ Pods/Target Support Files/Pods-Hustla/Pods-Hustla-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Hustla : NSObject
+@end
+@implementation PodsDummy_Pods_Hustla
+@end
